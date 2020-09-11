679e3d9dfae0c25476ca1c3520c36d6811cfa0e4
lionani07 <lionani07@gmail.com>
2020-09-10 23:12:26 -0300

    add lombok

--- src/main/java/br/com/alura/forum/model/Curso.java
+++ src/main/java/br/com/alura/forum/model/Curso.java
@@ -1,5 +1,12 @@
 package br.com.alura.forum.model;
 
+import lombok.*;
+
+@Builder(toBuilder = true)
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Curso {
 
 	private Long id;
@@ -8,55 +15,6 @@ public class Curso {
 
 	public Curso(String nome, String categoria) {
 		this.nome = nome;
-		this.categoria = categoria;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Curso other = (Curso) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public String getCategoria() {
-		return categoria;
-	}
-
-	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
 
