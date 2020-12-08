b75d7c208a37581746b2cd789ede08fd043d6f82
lionani07 <lionani07@gmail.com>
2020-12-08 20:39:30 -0300

    config security and block endpoints

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -0,0 +1,32 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Configuraçoes de autentizaçao...
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    }
+
+    // Configuraçoes de autorizaçao...
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+    }
+
+    // Configuraçoes de recursos estáticos...
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+    }
+}
