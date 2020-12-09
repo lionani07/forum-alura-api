a9cb12c7829e10a44dc36a29a7788340c7ba5a84
lionani07 <lionani07@gmail.com>
2020-12-08 21:33:25 -0300

    preparing userDetails and GrantedAuthority

--- src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfig.java
@@ -22,7 +22,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
-                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll();
+                .antMatchers(HttpMethod.GET, "/topicos/*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
     }
 
     // Configuraçoes de recursos estáticos...
--- src/main/java/br/com/alura/forum/model/Usuario.java
+++ src/main/java/br/com/alura/forum/model/Usuario.java
@@ -1,11 +1,13 @@
 package br.com.alura.forum.model;
 
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 @Builder(toBuilder = true)
@@ -13,7 +15,7 @@ import javax.persistence.Id;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Getter
-public class Usuario {
+public class Usuario implements UserDetails {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,4 +23,42 @@ public class Usuario {
 	private String nome;
 	private String email;
 	private String senha;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfis = new ArrayList<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.senha;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
